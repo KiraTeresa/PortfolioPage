7d13bb3d4f2238b2e03c00a0aec164bd1eeab8d8
Kira <kira.soba@gmx.de>
2022-12-22 22:07:55 +0100

    Change style for linkedIn and GitHub links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import './Footer.scss'
-import GitHub from '../../assets/images/GitHub_Logo_White.png'
-import LinkedIn from '../../assets/images/LI-Logo.png'
+// import GitHub from '../../assets/images/GitHub_Logo_White.png'
+// import LinkedIn from '../../assets/images/LI-Logo.png'
 
 function Footer() {
     return (
         <footer className="dark">
             <div className='footer-wrapper'>
-                <div>Kira Teresa Soba, visit me on </div>
+                <div>Kira Teresa Soba, visit me on</div>
                 <div>
-                    <a href="https://github.com/KiraTeresa"><img src={GitHub} alt="GitHub Logo" /></a>
-                    <a href="https://www.linkedin.com/in/kira-teresa-soba/"><img src={LinkedIn} alt="LinkedIn Logo" /></a>
+                    {/* <a href="https://github.com/KiraTeresa"><img src={GitHub} alt="GitHub Logo" /></a>
+                    <a href="https://www.linkedin.com/in/kira-teresa-soba/"><img src={LinkedIn} alt="LinkedIn Logo" /></a> */}
+                    <a href="https://www.linkedin.com/in/kira-teresa-soba/"><p className='zenDots'>Linkedin</p></a>
+                    <a href="https://github.com/KiraTeresa"><p className='zenDots'>GitHub</p></a>
                 </div>
             </div>
         </footer>
