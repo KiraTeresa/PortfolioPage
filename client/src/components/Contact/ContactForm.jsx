e0b1bcef83d6e82395bd96aee3117654c6c0198f
Kira <kira.soba@gmx.de>
2022-12-25 13:00:30 +0100

    Handle change and submit of contact form

--- client/src/components/Contact/ContactForm.jsx
+++ client/src/components/Contact/ContactForm.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 import './contact.scss'
+import apiClient from '../../services/apiClient'
 
 function ContactForm() {
+    const [data, setData] = useState({ sender: "", email: "", message: "" })
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        apiClient.post('/contact', data).then((result) => {
+            console.log(result.data)
+            setData({ sender: "", email: "", message: "" })
+        }).catch(err => console.log('Error: ', err))
+    }
+
     return (
         <div className='light'>
             <section className='form'>
                 <h2>Get in touch</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>Who are you?
-                        <input></input>
+                        <input name='sender' type='text' value={data.sender} onChange={handleChange}></input>
                     </label>
                     <label>What's your email address?
-                        <input></input>
+                        <input name='email' type='email' value={data.email} onChange={handleChange}></input>
                     </label>
                     <label>Your message for me
-                        <textarea></textarea>
+                        <textarea name='message' type='text' value={data.message} onChange={handleChange}></textarea>
                     </label>
-                    <button>send</button>
+                    <button type='submit'>send</button>
                 </form>
             </section>
         </div>
