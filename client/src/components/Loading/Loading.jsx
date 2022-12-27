02322eebde6fb8e83c247d28ba2e2005316f5fc5
Kira <kira.soba@gmx.de>
2022-12-27 23:40:15 +0100

    New loading animation done

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,23 @@
+import './loading.scss'
+
+const Loading = () => {
+  return (
+      <div className='loading-wrapper dark'>
+        <div>
+          <span className='zenDots'>K</span>
+          <span className='zenDots'>I</span>
+          <span className='zenDots'>R</span>
+          <span className='zenDots'>A</span>
+          <span className='zenDots'>T</span>
+          <span className='zenDots'>E</span>
+          <span className='zenDots'>R</span>
+          <span className='zenDots'>E</span>
+          <span className='zenDots'>S</span>
+          <span className='zenDots'>A</span>
+        </div>
+        <div>loading.</div>
+      </div>
+  );
+};
+
+export default Loading;
