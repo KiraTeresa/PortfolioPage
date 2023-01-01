258acbabede79739cb45e1d3ac93ee793173d0bd
Kira <kira.soba@gmx.de>
2023-01-01 20:36:20 +0100

    Set env

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -7,17 +7,29 @@ router.post('/', async (req, res, next) => {
 
     // prepare data
     const data = {
-        from: 'my@portfoliopage.com',
-        to: 'kira@kira.com',
+        from: email,
+        to: process.env.MAIL_ME,
         subject: `${sender} via contact form`,
         text: message,
-        html: `<b>${message}</b>`
+        html: `<p><b>Name:</b> ${sender}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b> ${message}</p>`
     }
 
     // use mailtrap to create transporter
+    // let transporter = nodemailer.createTransport({
+    //     host: "smtp.mailtrap.io",
+    //     port: 2525,
+    //     auth: {
+    //         user: process.env.MAIL_USER,
+    //         pass: process.env.MAIL_PASS
+    //     }
+    // });
+
+    // use mail to create transporter
     let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
@@ -26,7 +38,7 @@ router.post('/', async (req, res, next) => {
     
     // send via nodemailer
     await transporter.sendMail(data).then(info => {
-        console.log('send>> ', info);
+        // console.log('send>> ', info);
         res.json('Your message has been send to Kira.')
     })
     .catch(error => console.log(error))
