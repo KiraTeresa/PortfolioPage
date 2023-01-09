60a104b2b79ed688d573cf6cc2461a21cb884a31
Kira <kira.soba@gmx.de>
2023-01-09 21:34:56 +0100

    Style: adjust positioning of homepage info

--- client/src/components/About/Introduction.jsx
+++ client/src/components/About/Introduction.jsx
@@ -3,13 +3,10 @@ import './about.scss'
 function Introduction({ isVisible }) {
     return (
         <section className={`introduction ${isVisible ? 'visible' : 'hidden'}`}>
-            {/* <div className='img-wrapper'>
-                <img src={MeImage} alt="Kira Teresa Soba" />
-            </div> */}
-            {/* <div className={`text ${isVisible ? "" : "hide"}`}> */}
-            <h2 className='slideOut'>Hi. I'm Kira</h2>
-            <h2 className='zenDots slideOut'>Fullstack Web Developer</h2>
-            {/* </div> */}
+            <div>
+                <h2 className='slideOut'>Hi. I'm Kira</h2>
+                <h2 className='zenDots slideOut'>Fullstack Web Developer</h2>
+            </div>
         </section >
     )
 }
